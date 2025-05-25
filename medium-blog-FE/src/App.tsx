57fbd3d05310158cbd074946549bd80694936e98
Javed Akhtar <javedakhtarsofficial@gmail.com>
2025-05-25 14:54:18 +0530

    FE Updated

--- medium-blog-FE/src/App.tsx
+++ medium-blog-FE/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import SignIn from "./pages/SignIn"
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
